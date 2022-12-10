3b3552de192e722d2597c6119ce0161730a9d1f4
Ty Lewis <lewis.clay.ty@gmail.com>
2022-12-10 14:12:06 -0600

    Cleaned up some path-specific code

--- PlumeSimulator/Gaussian/GPlume.m
+++ PlumeSimulator/Gaussian/GPlume.m
@@ -1,52 +1,85 @@
-function [PlumeD,SigmaY,str] = LoadPlumeGaussianStability(plumecase)
-% Fixed: Static Gaussian Plumes - 
+function [PlumeD,SigmaY,str] = GPlume(plumecase)
+% Cases: Static Gaussian Plumes - 
 %         Fixed Ground Source, 
 %           Wind Velocity = [0,2,0], 
 %             Diffusion constant: 0.5
+% 1: Advection constant: 1  (l=3,L=10)
+% 2: Advection constant: 2  (l=4,L=20)
+% 3: Advection constant: 2.5  (l=5,L=25)
+% 4-9: Stability Class Tests A-F
 
 plumecase = num2str(plumecase);
 
     switch(plumecase)
+
+    case '1'
+        load('PlumeSimulator/Gaussian/CaseData/case1_C.mat');
+        load('PlumeSimulator/Gaussian/CaseData/case1_u.mat');
+        load('PlumeSimulator/Gaussian/CaseData/case1_v.mat');
+        l = 3;
+        L = 10;
         
-   case '1'
-        path = 'home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/';
-        str = 'Q10-U3-ClassA-rural-CO2';
-        load("/home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/Q10-U3-ClassA-rural-CO2PlumeD.mat");
-        load("/home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/Q10-U3-ClassA-rural-CO2SigmaY.mat");
+    case '2'
+        load('PlumeSimulator/Gaussian/CaseData/case2_C.mat');
+        load('PlumeSimulator/Gaussian/CaseData/case2_u.mat');
+        load('PlumeSimulator/Gaussian/CaseData/case2_v.mat');
+        l = 3; % default: 4
+        L = 15; % default: 20
         
-   case '2'
-        path = 'home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/';
-        str = 'Q10-U3-ClassB-rural-CO2';
-        load("/home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/Q10-U3-ClassB-rural-CO2PlumeD.mat");
-        load("/home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/Q10-U3-ClassB-rural-CO2SigmaY.mat");
-   
-   case '3'
-        path = 'home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/';
-        str = 'Q10-U3-ClassC-rural-CO2';
-        load("/home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/Q10-U3-ClassC-rural-CO2PlumeD.mat");
-        load("/home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/Q10-U3-ClassC-rural-CO2SigmaY.mat");
+    case '3'
+        load('PlumeSimulator/Gaussian/CaseData/case3_C.mat');
+        load('PlumeSimulator/Gaussian/CaseData/case3_u.mat');
+        load('PlumeSimulator/Gaussian/CaseData/case3_v.mat');
+        l = 3; % default: 5
+        L = 20; % default: 25
         
-   case '4'
-        path = 'home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/';
-        str = 'Q10-U3-ClassD-rural-CO2';
-        load("/home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/Q10-U3-ClassD-rural-CO2PlumeD.mat");
-        load("/home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/Q10-U3-ClassD-rural-CO2SigmaY.mat");
-   
+    case '4'
+        path = 'PlumeSimulator/Gaussian/ATDmodel/Models/';
+        str = 'Q1-U3-ClassA-rural-CO2';
+        load(path+str+'PlumeD.mat');
+        load(path+str+'SigmaY.mat');
+        
    case '5'
-        path = 'home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/';
-        str = 'Q10-U3-ClassE-rural-CO2';
-        load("/home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/Q10-U3-ClassE-rural-CO2PlumeD.mat");
-        load("/home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/Q10-U3-ClassE-rural-CO2SigmaY.mat");
-        
+        path = 'PlumeSimulator/Gaussian/ATDmodel/Models/';
+        str = 'Q1-U3-ClassB-rural-CO2';
+        load(path+str+'PlumeD.mat');
+        load(path+str+'SigmaY.mat');
+   
    case '6'
-        path = 'home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/';
-        str = 'Q10-U3-ClassF-rural-CO2';
-        load("/home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/Q10-U3-ClassF-rural-CO2PlumeD.mat");
-        load("/home/ty/MSNsimulator/GaussianPlumeSimulator/ATDmodel/Models/Q10-U3-ClassF-rural-CO2SigmaY.mat");
+        path = 'PlumeSimulator/Gaussian/ATDmodel/Models/';
+        str = 'Q1-U3-ClassC-rural-CO2';
+        load(path+str+'PlumeD.mat');
+        load(path+str+'SigmaY.mat');
         
+   case '7'
+        path = 'PlumeSimulator/Gaussian/ATDmodel/Models/';
+        str = 'Q1-U3-ClassD-rural-CO2';
+        load(path+str+'PlumeD.mat');
+        load(path+str+'SigmaY.mat');
+   
+   case '8'
+        path = 'PlumeSimulator/Gaussian/ATDmodel/Models/';
+        str = 'Q1-U3-ClassE-rural-CO2';
+        load(path+str+'PlumeD.mat');
+        load(path+str+'SigmaY.mat');
         
+   case '9'
+        path = 'PlumeSimulator/Gaussian/ATDmodel/Models/';
+        str = 'Q1-U3-ClassF-rural-CO2';
+        load(path+str+'PlumeD.mat');
+        load(path+str+'SigmaY.mat');     
+
         otherwise
             
     end
-    
+
+    PlumeFinal = flipud(PlumeD(:,:)); 
+    Vf = PlumeFinal/(MaxC);  % Normalized starting and ending plume visual
+    T = 5;          
+    Area = 1000;            Mid = Area/2;       
+    ResX = 1;            ResY = 1;
+    OffsetX = Mid/ResX;   OffsetY = 250;
+    gasMap = robotics.OccupancyGrid(Vf,1/ResX);      
+    gasMap.GridLocationInWorld = [0,-OffsetY]; % Create map for robot visualizer
+
 end
--- Update/configParameters.m
+++ Update/configParameters.m
@@ -18,52 +18,50 @@ metric = 0; % Metric test cases (trial 0 for default values)
 algorithm = "GA"; %  'BB" - "GA" - "GAv2" - "APS" - "Hybrid" - "PDE"
 
 % path to save results in:
-path = "MSNsimulator/TestResults/PlumeCases/Case" + plumecase + "/Test" + testcase + "/";
+path = "MSNsimulator/Results/PlumeCases/Case" + plumecase + "/Test" + testcase + "/";
 str = algorithm + "case" + plumecase + "test" + testcase + "trial" + trial + "N" + N + "metric" + metric;
 
-
-
-
 %% Plume Model Configuration:
-dynamicPlume = 0; % plume concentration updates over time?
-turbulence = 1; % is turbulence included? for visualizing results after trial
-%[l,L,PlumeD,WindX,WindD] = plumeShape(plumecase); % add T,Ts,Res to output for dynamic plumes
-[PlumeD,SigmaY,Sclass] = GPlume(plumecase);
+dynamicPlume = 0; % plume concentration moves over time?
+turbulence = 1; % is turbulence included? 
+
+if turbulence == 1
+    [l,L,PlumeD,WindX,WindD] = plumeShape(plumecase); % add T,Ts,Res to output for dynamic plumes
+else
+    [PlumeD,SigmaY,Sclass] = GPlume(plumecase);
+end
+
 [T,ResX,ResY,OffsetX,OffsetY,gasMap] = GplumeParameters(plumecase,PlumeD);
-MaxC = max(max(max(PlumeD)));
+MaxC = max(max(max(PlumeD))); % max concentration
 
 %% Spawn MSN:
 % add Tmax to spawnmsn
 [initX,initY,initAngle,Tmax] = SpawnMSN(testcase); % Initialize Spawn point and starting goal
 origin = [initX,initY]; % spawn point
-goalpoints = origin; % initialize goalpoint
-init = [initX - 5,initY];
-distError = sqrt((initX^2)+(initY^2)); % Initial displacement
+goalpoints = origin; % initialize goalpoint at origin
+init = [initX - 5,initY]; % vehicle spawn position (offset 5 meters)
+distError = sqrt((initX^2)+(initY^2)); % Initial displacement from source
 
-%% Gradient Ascent (GA):
-[mag,sensorDist,distThresh] = GradientAscentMetrics(trial);
-%GAmetrics = [mag,sensorDist,distThresh,N];
+if algorithm == 'GA'
+    [mag,sensorDist,distThresh] = GradientAscentMetrics(trial);
+    %GAmetrics = [mag,sensorDist,distThresh,N];
+else if algorithm == 'APS'
+    [Expand,LimitSize,InitSize] = APSmetrics(metric); 
+    Shrink = 1/Expand;
+    [waypoints,finalPoint] = initPathPoints(trial,init,InitSize); % Path Selector
+    %APSmetrics = [Expand,LimitSize];
+    pathUpdates = [];
+else
+    %
+end
 
-%% Adaptive Peak Seeking (APS):
-% add initSize metric
-[Expand,LimitSize,InitSize] = APSmetrics(metric);
-%Expand = 2;    LimitSize = 3;   
-Shrink = 1/Expand;
-[waypoints,finalPoint] = initPathPoints(trial,init,InitSize); % Path Selector
-%APSmetrics = [Expand,LimitSize];
-
-pathUpdates = [];
-
-%[waypoints,finalPoint] = initWaypoints(trial,init); % Path Selector
-%[Expand,LimitSize] = APSmetrics(trial);
-
-%% Other
-% Gazebo and Simulink plume origin coordinates
+%% Domain parameters
 Area = 1000;      
 Mid = Area/2; 
 Res = 1;   
 Offset = [OffsetX,OffsetY];
+
+% Gazebo and Simulink plume origin coordinates
 %x_g = 0;      x = (x_g+OffsetX);
 %y_g = 0;      y = (y_g+OffsetY);
-
 gazeboPause;
